8b6ccb76431d1f1ad32c603807919ac2df86d6c4
Luciano Guzinski <lucianoguzinski@gmail.com>
2016-05-16 01:41:48 -0300

    Bundle principal criado

---
+++ src/DesportoBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DesportoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('DesportoBundle:Default:index.html.twig');
+    }
+}
---
+++ src/DesportoBundle/DesportoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DesportoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DesportoBundle extends Bundle
+{
+}
