ef27bfa858e0ddbc268d7aee1c1f89790cb5c965
Luciano Guzinski <lucianoguzinski@gmail.com>
2016-06-23 01:06:50 -0300

    Algumas modificações de modelagem

--- src/DesportoBundle/Entity/FaseClassificatoria.php
+++ src/DesportoBundle/Entity/FaseClassificatoria.php
@@ -17,10 +17,9 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class FaseClassificatoria
 {
-    
     const OITAVAS = "OITAVAS";
     const QUARTAS = "QUARTAS";
-    const SEMI = "SEMI";
+    const SEMIFINAL = "SEMIFINAL";
     const FINAL_ = "FINAL";
 
     /**
@@ -32,10 +31,86 @@ class FaseClassificatoria
      */
     private $id;
     
+    /**
+     *  @ORM\Column(type="string", columnDefinition="ENUM('O', 'Q', 'S', 'F')", nullable=false)
+     */
+    private $tipo;
+
+    /**
+     *  @ORM\Column(name="jogo_unico", type="boolean", nullable=false)
+     */
+    private $jogoUnico = FALSE;
+
+    /**
+     * @var FaseClassificatoria
+     *
+     * @ORM\ManyToOne(targetEntity="FaseClassificatoria", inversedBy="fasesPai")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="fase_filha", referencedColumnName="id")
+     * })
+     */
+    private $faseFilha;
+            
+    /**
+     * @var Collection
+     * @ORM\OneToMany(targetEntity="FaseClassificatoria", mappedBy="faseFilha")
+     **/
+    private $fasesPai;
+
+
     public function getId()
     {
         return $this->id;
     }
+    
+    
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    public function getJogoUnico()
+    {
+        return $this->jogoUnico;
+    }
+
+    public function getFaseFilha()
+    {
+        return $this->faseFilha;
+    }
+
+    public function getFasesPai()
+    {
+        return $this->fasesPai;
+    }
+
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+        return $this;
+    }
+
+    public function setJogoUnico($jogoUnico)
+    {
+        $this->jogoUnico = $jogoUnico;
+        return $this;
+    }
+
+    public function setFaseFilha(FaseClassificatoria $faseFilha)
+    {
+        $this->faseFilha = $faseFilha;
+        return $this;
+    }
+
+    public function setFasesPai(Collection $fasesPai)
+    {
+        $this->fasesPai = $fasesPai;
+        return $this;
+    }
+
+    
+
+    
 
 
 }
